665e124d9bb38fd488a2a3d3484d18fd9a25d574
Bhupesh Kumar <bhupeshkr2912@gmail.com>
2023-10-30 18:58:42 +0530

    update

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ mongoose.connect(process.env.MONGO_URL, {
   // Middleware
   app.use(cors({
     // origin:"https://globalsuccesspoint.netlify.app"
-    // origin:"https://powerfullindia.com",
-    origin:"https://www.powerfullindia.com",
+    origin:"https://powerfullindia.com",
+    // origin:"https://www.powerfullindia.com",
     // origin:"http://localhost:3000",
   }));
   cloudinaryConfig();
