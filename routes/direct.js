577b7a27012c2e034c1c4ff7d45c05f0f53f617d
BhupeshB7 <bhupeshkr2912@gmail.com>
2024-05-14 09:09:43 +0530

    update direct member

--- routes/direct.js
+++ routes/direct.js
@@ -6,21 +6,36 @@ const User = require('../models/User');
 router.get('/direct/:userId', async (req, res) => {
   try {
     const { userId } = req.params;
-    const user = await User.findOne({ userId: userId }).lean();
+    const { page = 1, limit = 5, sortField = 'name', sortOrder = 'asc'  } = req.query;
+
+    const user = await User.findOne({ userId }).lean();
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    const sponsor = await User.find({ sponsorId: user.userId });
+    const totalSponsors = await User.countDocuments({ sponsorId: userId });
+    const totalPages = Math.ceil(totalSponsors / limit);
 
-    if (!sponsor) {
-      return res.status(404).json({ error: 'Sponsor not found' });
+    const sponsors = await User.find({ sponsorId: userId })
+      .sort({ [sortField]: sortOrder })
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .lean();
+
+    if (!sponsors || sponsors.length === 0) {
+      return res.status(404).json({ error: 'Sponsors not found' });
     }
 
-    res.status(200).json(sponsor);
+    res.status(200).json({
+      currentPage: parseInt(page),
+      lastPage: totalPages,
+      totalPage: totalPages,
+      totalData: totalSponsors,
+      data: sponsors
+    });
   } catch (err) {
-    // console.error(err);
+    console.error(err);
     res.status(500).json({ error: 'Server Error' });
   }
 });
